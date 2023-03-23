9514a0321f5face4eb84423189c1507815032d82
Chikaodili Deng <mchikky@yahoo.com>
2023-03-23 09:49:39 +0100

    Initial Commit

---
+++ Xplore_Interface2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Xplore_Interface2
+//
+//  Created by Chikaodili Deng on 23/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Xplore_Interface2/Xplore_Interface2App.swift
@@ -0,0 +1,17 @@
+//
+//  Xplore_Interface2App.swift
+//  Xplore_Interface2
+//
+//  Created by Chikaodili Deng on 23/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Xplore_Interface2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
